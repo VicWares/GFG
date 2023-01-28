8cd8068640354139864513e59ea226037e648c7b
Vic Wintriss <vic@wintrisstech.org>
2023-01-27 21:39:44 -0800

    GLG version 230127 GLG working with LearningDL4J showing start button

--- src/main/java/org/example/GFG.java
+++ src/main/java/org/example/GFG.java
@@ -1,23 +1,40 @@
-package org.example;
+package org.example;// Java Program to Illustrate Working of SwingWorker Class
+
+// Importing required classes
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.geom.Dimension2D;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-import javax.swing.*;
 public class GFG
 {
     private static JLabel statusLabel;
     private static JFrame mainFrame;
+    private static int FEATURES_COUNT = 4;
+    private static int CLASSES_COUNT = 3;
+    private static String version = "230127";
+    private static Object eval;
+    private int x = 100;
+    private int y = 100;
+    private double accuracy;
+    private ArrayList<Dimension> graphPointsList = new ArrayList<>();
+    private Dimension2D oldPoint = new Dimension(0,0);
+    private Dimension2D newPoint = new Dimension(0,0);;
+    private int i;
+    static LearningDL4J learningDL4J = new LearningDL4J();
     public static void swingWorkerSample()
     {
+        LearningDL4J learningDL4J = new LearningDL4J();
         mainFrame = new JFrame("Swing Worker");
         mainFrame.setSize(400, 400);
         mainFrame.setLayout(new GridLayout(2, 1));
-        mainFrame.addWindowListener(new WindowAdapter()
-        {
+        mainFrame.addWindowListener(new WindowAdapter() {
+            // Method
             @Override
             public void windowClosing(WindowEvent e)
             {
@@ -26,14 +43,14 @@ public class GFG
         });
         statusLabel = new JLabel("Not Completed", JLabel.CENTER);
         mainFrame.add(statusLabel);
-        JButton btn = new JButton("Start counter");
+        JButton btn = new JButton("Start LearningDL");
         btn.setPreferredSize(new Dimension(5, 5));
-        btn.addActionListener(new ActionListener()
-        {
+        btn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                System.out.println("Button clicked, thread started");
+                System.out.println(
+                        "Button clicked, thread started");
                 startThread();
             }
         });
@@ -42,23 +59,25 @@ public class GFG
     }
     private static void startThread()
     {
-        SwingWorker sw1 = new SwingWorker()
-        {
+        SwingWorker sw1 = new SwingWorker() {
             @Override
-            protected String doInBackground() throws Exception
+            protected String doInBackground() throws Exception//************************> Defining what thread will do here
             {
                 // Defining what thread will do here
-                for (int i = 10; i >= 0; i--) {
-                    Thread.sleep(100);
-                    System.out.println("Value in thread : "
-                            + i);
-                    publish(i);
-                }
+//                for (int i = 10; i >= 0; i--) {
+//                    Thread.sleep(100);
+//                    System.out.println("Value in thread : "
+//                            + i);
+//                    publish(i);
+//                }
+                LearningDL4J learningDL4J = new LearningDL4J();
+                learningDL4J.loadData();
                 String res = "Finished Execution";
                 return res;
             }
-            @Override
-            protected void process(List chunks)
+
+            // Method
+            @Override protected void process(List chunks)
             {
                 // define what the event dispatch thread
                 // will do with the intermediate results
@@ -66,27 +85,31 @@ public class GFG
                 int val = (int) chunks.get(chunks.size() - 1);
                 statusLabel.setText(String.valueOf(val));
             }
-            @Override
-            protected void done()
+
+            // Method
+            @Override protected void done()
             {
                 // this method is called when the background
                 // thread finishes execution
                 try {
                     String statusMsg = (String) get();
-                    System.out.println("Inside done function");
+                    System.out.println(
+                            "Inside done function");
                     statusLabel.setText(statusMsg);
-                } catch (InterruptedException e)
-                {
+                }
+                catch (InterruptedException e) {
                     e.printStackTrace();
-                } catch (ExecutionException e)
-                {
+                }
+                catch (ExecutionException e) {
                     e.printStackTrace();
                 }
             }
         };
+
         // Executes the swingworker on worker thread
         sw1.execute();
     }
+    // Main driver method
     public static void main(String[] args)
     {
         swingWorkerSample();
---
+++ src/main/java/org/example/LearningDL4J.java
@@ -0,0 +1,94 @@
+package org.example;
+/*****************************************************************************************
+ * DL4J Example: version 220127
+ *****************************************************************************************/
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.SplitTestAndTrain;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
+import org.nd4j.linalg.io.ClassPathResource;
+import org.nd4j.linalg.learning.config.Nesterovs;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Dimension2D;
+import java.util.ArrayList;
+import static java.lang.System.out;
+public class LearningDL4J extends JComponent
+{
+    private static int FEATURES_COUNT = 4;
+    private static int CLASSES_COUNT = 3;
+    private static String version = "230127";
+    private static Object eval;
+    private static int i;
+    private int x = 100;
+    private int y = 100;
+    private static double accuracy;
+    private ArrayList<Dimension> graphPointsList = new ArrayList<>();
+    private static Dimension2D oldPoint = new Dimension(0,0);
+    private static Dimension2D newPoint = new Dimension(0,0);;
+    void loadData()
+    {
+        try (RecordReader recordReader = new CSVRecordReader(0, ',')) {
+            recordReader.initialize(new FileSplit(new ClassPathResource("iris.csv").getFile()));
+            DataSetIterator iterator = new RecordReaderDataSetIterator(recordReader, 150, 4, 3);
+            DataSet allData = iterator.next();
+            allData.shuffle(123);
+            DataNormalization normalizer = new NormalizerStandardize();
+            normalizer.fit(allData);
+            normalizer.transform(allData);
+            SplitTestAndTrain testAndTrain = allData.splitTestAndTrain(0.65);
+            DataSet trainingData = testAndTrain.getTrain();
+            DataSet testingData = testAndTrain.getTest();
+            irisNNetwork(trainingData, testingData);
+        } catch (Exception e) {
+            out.println("Error: " + e.getLocalizedMessage());
+        }
+    }
+    private static void irisNNetwork(DataSet trainingData, DataSet testData)
+    {
+        MultiLayerConfiguration configuration = new NeuralNetConfiguration.Builder()
+                .activation(Activation.TANH)
+                .weightInit(WeightInit.XAVIER)
+                .updater(new Nesterovs(0.1, 0.9))
+                .l2(0.0001)
+                .list()
+                .layer(0, new DenseLayer.Builder().nIn(FEATURES_COUNT).nOut(3).build())
+                .layer(1, new DenseLayer.Builder().nIn(3).nOut(3).build())
+                .layer(2, new OutputLayer.Builder(
+                        LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD).activation(Activation.SOFTMAX)
+                        .nIn(3).nOut(CLASSES_COUNT).build())
+                .backprop(true).pretrain(false)
+                .build();
+        MultiLayerNetwork model = new MultiLayerNetwork(configuration);
+        model.init();
+        Evaluation eval = null;
+        for (i = 0; i < 1000; i++)
+        {
+            INDArray output = model.output(testData.getFeatureMatrix());
+            eval = new Evaluation(3);
+            model.fit(trainingData);
+            eval.eval(testData.getLabels(), output);
+            accuracy = eval.accuracy();
+            newPoint = new Dimension(i, (int) (accuracy * 1000));
+            out.print("\nAccuracy " + accuracy + " " + newPoint.getWidth() + " " + newPoint.getHeight());
+            oldPoint = newPoint;
+        }
+        out.printf(eval.stats());
+    }
+}
+
